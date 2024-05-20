42d0fc863a6174c4b09fd21fc176b880b4272d94
SkSamuel157 <samuelscs12senai@gmail.com>
2024-05-20 17:01:51 -0300

    completed

--- mercado/App.js
+++ mercado/App.js
@@ -1,96 +1,82 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import {useState} from 'react'
-import { StyleSheet, Text, View, TouchableOpacity, Button } from 'react-native';
-import { Entypo } from '@expo/vector-icons'; 
-
-
+import { useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default function App() {
-  const [darkMode, setDarkMode] = useState(false)
-  const buttons = ['AC', 'DEL', '%', '/', 7, 8, 9, '*', 4, 5, 6, '-', 3, 2, 1, '+', 0, '.', '+/-', '=']  
+  const buttons = ['AC', 'DEL', '%', '/', 7, 8, 9, '*', 4, 5, 6, '-', 3, 2, 1, '+', 0, '.', '+/-', '='];
 
-  const [currentNumber, setCurrentNumber] = useState("")
-  const [lastNumber, setLastNumber] = useState("")
+  const [currentNumber, setCurrentNumber] = useState("");
+  const [lastNumber, setLastNumber] = useState("");
 
+  function calculator() {
+    const splitNumbers = currentNumber.split(' ');
+    const firstNumber = parseFloat(splitNumbers[0]);
+    const lastNumber = parseFloat(splitNumbers[2]);
+    const operator = splitNumbers[1];
 
-  function calculator(){
-    const splitNumbers = currentNumber.split(' ')
-    const fistNumber = parseFloat(splitNumbers[0])
-    const lastNumber = parseFloat(splitNumbers[2])
-    const operator = splitNumbers[1]
-
-    switch(operator){
+    switch (operator) {
       case '+':
-        setCurrentNumber((fistNumber + lastNumber).toString())
-        return
-      case '-': 
-        setCurrentNumber((fistNumber - lastNumber).toString())
-        return
+        setCurrentNumber((firstNumber + lastNumber).toString());
+        return;
+      case '-':
+        setCurrentNumber((firstNumber - lastNumber).toString());
+        return;
       case '*':
-        setCurrentNumber((fistNumber * lastNumber).toString())
-        return
-      case '/': 
-        setCurrentNumber((fistNumber / lastNumber).toString())
-        return
+        setCurrentNumber((firstNumber * lastNumber).toString());
+        return;
+      case '/':
+        setCurrentNumber((firstNumber / lastNumber).toString());
+        return;
     }
   }
 
-  function handleInput(buttonPressed){
-    console.log(buttonPressed)
-    if(buttonPressed === '+' | buttonPressed === "-" | buttonPressed === "*" | buttonPressed === "/" ){
-      setCurrentNumber(currentNumber + " " + buttonPressed + " ")
-      return
+  function handleInput(buttonPressed) {
+    console.log(buttonPressed);
+    if (buttonPressed === '+' || buttonPressed === "-" || buttonPressed === "*" || buttonPressed === "/") {
+      setCurrentNumber(currentNumber + " " + buttonPressed + " ");
+      return;
     }
-    switch(buttonPressed){
+    switch (buttonPressed) {
       case 'DEL':
-        setCurrentNumber(currentNumber.substring(0, (currentNumber.length -1)))
-        return
+        setCurrentNumber(currentNumber.substring(0, (currentNumber.length - 1)));
+        return;
       case 'AC':
-        setLastNumber("")
-        setCurrentNumber("")
-        return
+        setLastNumber("");
+        setCurrentNumber("");
+        return;
       case '=':
-        setLastNumber(currentNumber + " = ")
-        calculator()
-        return
+        setLastNumber(currentNumber + " = ");
+        calculator();
+        return;
       case '+/-':
-        return
+        return;
     }
 
-    setCurrentNumber(currentNumber + buttonPressed)
+    setCurrentNumber(currentNumber + buttonPressed);
   }
-
+  
   const styles = StyleSheet.create({
     results: {
-      backgroundColor: darkMode ? "#282f3b" : "#F2A74B",
+      backgroundColor: "#F2A74B", // cor de fundo modificada
       width: '100%',
       minHeight: 280,
       alignItems: 'flex-end',
-      justifyContent: 'flex-end'
+      justifyContent: 'flex-end',
+      borderBottomRightRadius: 50,
+      borderBottomLeftRadius: 50,
+      marginBottom: 10,
     },
     resultText: {
-      color: darkMode ? "#f5f5f5" : "#282F38",
-      margin: 10,
-      fontSize: 40
+      color: "#591902", // cor do texto modificada
+      margin: 20,
+      fontSize: 70,
     },
-
-    historyText:{
-      color: darkMode ? "#B5B7BB" : "#7c7c7c",
-      fontSize: 20,
+    historyText: {
+      color: "#591902", // cor do texto do histórico modificada
+      fontSize: 25,
       marginRight: 10,
       alignSelf: 'flex-end',
-    },
-    themeButton: {
-      alignSelf: 'flex-start',
-      bottom: 120,
-      margin: 10,
-      backgroundColor: darkMode ? "#7b8084" :"#e5e5e5",
-      alignItems: 'center',
-      justifyContent: 'center',
-      width: 50,
-      height: 50,
-      borderRadius: 25,
       
     },
     buttons: {
@@ -98,41 +84,43 @@ export default function App() {
       flexWrap: 'wrap',
     },
     button: {
-      borderColor: darkMode ? '#3f4d5b' : "#e5e5e5",
+      borderColor: "#D9BC9A",
       borderWidth: 1,
       alignItems: 'center',
       justifyContent: 'center',
-      minWidth: 90, 
-      minHeight: 90,
+      minWidth: 95 ,
+      minHeight: 80,
       flex: 2,
+      backgroundColor: "#F2994A", // cor de fundo dos botões modificada
+      borderRadius: 50,
     },
     textButton: {
-      color: darkMode ? "#b5b7bb" : "#7c7c7c",
-      fontSize: 20,
-    }, 
+      color: "#FFFFFF", // cor do texto dos botões modificada
+      fontSize: 30,
+    },
+    home:{
+      backgroundColor: "#D9BC9A",
+    },
   });
-
+  
   return (
-    <View>
+    <View style={styles.home}>
       <View style={styles.results}>
-        <TouchableOpacity style={styles.themeButton}>
-          <Entypo name={darkMode ? "light-up" : 'moon'} size={24} color={darkMode ? "white" : 'black'} onPress={() => darkMode ? setDarkMode(false) : setDarkMode(true)} />
-        </TouchableOpacity>
         <Text style={styles.historyText}>{lastNumber}</Text>
         <Text style={styles.resultText}>{currentNumber}</Text>
         <StatusBar style="auto" />
       </View>
       <View style={styles.buttons}>
-        {buttons.map((button) => 
+        {buttons.map((button) =>
           button === '=' ?
-        <TouchableOpacity onPress={() => handleInput(button)} key={button} style={[styles.button, {backgroundColor: '#9DBC7B'}]}>
-          <Text style={[styles.textButton, {color: "white", fontSize: 30}]}>{button}</Text>
-        </TouchableOpacity>
-          :
-          <TouchableOpacity onPress={() => handleInput(button)} key={button} style={[styles.button, 
-          {backgroundColor: typeof(button) === 'number' ? darkMode === true ? '#303946' : '#fff': darkMode === true ? '#414853' : '#ededed'}]}>
-            <Text style={styles.textButton}>{button}</Text>
-          </TouchableOpacity>
+            <TouchableOpacity onPress={() => handleInput(button)} key={button} style={[styles.button, { backgroundColor: '#F2994A' }]}>
+              <Text style={[styles.textButton, { color: "white", fontSize: 50 }]}>{button}</Text>
+            </TouchableOpacity>
+            :
+            <TouchableOpacity onPress={() => handleInput(button)} key={button} style={[styles.button,
+            { backgroundColor: typeof (button) === 'number' ? '#A95F34' : '#591902' }]}>
+              <Text style={styles.textButton}>{button}</Text>
+            </TouchableOpacity>
         )}
       </View>
     </View>
