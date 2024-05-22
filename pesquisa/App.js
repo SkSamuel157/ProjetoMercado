d7471a992ddfffb26cefb50a5dfc981aac7e5b7b
SkSamuel157 <samuelscs12senai@gmail.com>
2024-05-22 17:10:33 -0300

    feito

--- pesquisa/App.js
+++ pesquisa/App.js
@@ -1,20 +1,27 @@
 import React from 'react';
-import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Image, Text, TouchableOpacity, StyleSheet } from 'react-native';
 
-const ProductPage = () => {
+const App = () => {
   return (
     <View style={styles.container}>
-      <View style={styles.header}>
-        <View style={styles.logoPlaceholder} />
-      </View>
+      <Image source={require('./img/logo.png')} style={styles.logo} />
+      <View style={styles.separator} />
       <View style={styles.productContainer}>
-        <Image source={require('')} style={styles.productImage}></Image>
+        <Image source={require('./img/Pao.png')} style={styles.productImage} />
+        <View style={styles.separator} />
         <Text style={styles.productName}>Pão de forma - Panco Premium</Text>
-        <Text style={styles.productShipping}>Envio Nacional</Text>
-        <Text style={styles.oldPrice}>R$ 105,99</Text>
+        <Text style={styles.shippingInfo}>Envio Nacional</Text>
+        <Text style={styles.oldPrice}>(R$ 105,99)</Text>
         <Text style={styles.newPrice}>R$ 13,99</Text>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>Comprar</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>Adicionar ao Carrinho</Text>
+          </TouchableOpacity>
+        </View>
       </View>
-
     </View>
   );
 };
@@ -24,56 +31,67 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 16,
     backgroundColor: '#D9BC9A',
-  },
-  header: {
-    backgroundColor: '#f5f5f5',
-    padding: 16,
     alignItems: 'center',
+  },
+  logo: {
+    width: '100%',
+    height: 75,
+    resizeMode: 'contain',
     marginBottom: 16,
   },
-  logoPlaceholder: {
+  separator: {
+    height: 4,
     width: '100%',
-    height: 50,
-    backgroundColor: '#e07a5f',
+    backgroundColor: '#591902', // Cor laranja
     marginBottom: 16,
   },
   productContainer: {
-    alignItems: 'center',
-    backgroundColor: '#fff',
+    width: '100%',
     padding: 16,
+    backgroundColor: '#F2A74B', // Cor de fundo do contêiner do produto
     borderRadius: 8,
-    elevation: 3,
   },
   productImage: {
-    width: 150,
+    width: '100%',
     height: 300,
     resizeMode: 'contain',
     marginBottom: 16,
   },
   productName: {
-    fontSize: 18,
+    fontSize: 16,
     fontWeight: 'bold',
-    textAlign: 'center',
     marginBottom: 8,
-    color: '#4A4A4A',
   },
-  productShipping: {
-    fontSize: 14,
+  shippingInfo: {
     color: 'green',
     marginBottom: 8,
   },
   oldPrice: {
-    fontSize: 16,
     textDecorationLine: 'line-through',
-    color: 'gray',
+    color: '#231240',
     marginBottom: 8,
   },
   newPrice: {
-    fontSize: 24,
+    fontSize: 25,
     fontWeight: 'bold',
-    color: '#e07a5f',
-    marginBottom: 16,
+    color: '#231240',
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 16,
+  },
+  button: {
+    backgroundColor: '#591902', // Cor laranja para os botões
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+    marginHorizontal: 5,
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'bold',
   },
 });
 
-export default ProductPage;
+export default App;
